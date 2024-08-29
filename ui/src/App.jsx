31ad2504355632ad7f09ff66f626193c4db20ab0
ArenaMendrika <hei.arena.3@gmail.com>
2024-08-30 01:02:38 +0300

    feat: add path to app.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,33 +1,30 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import PatrimoinePage from "./workplace/pages/PatrimoinePage";
-import ListPossessionPage from "./workplace/pages/ListPossessionPage";
-import CreatePossessionPage from "./workplace/pages/CreatePossessionPage";
-import UpdatePossessionPage from "./workplace/pages/UpdatePossessionPage";
+import PossessionListPage from "./workplace/pages/PossessionListPage";
+import CreatePossessionPage from "./workplace/pages/CreationPossession";
+import UpdatePossessionPage from "./workplace/pages/PossessionUpdate";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <header className="p-2 bg-secondary">
-        <div className="d-flex justify-content-center align-items-center">
-          <div>
-            <h1 className="text-light">Patrimoine Economique</h1>
+      <header className="p-2" style={{ backgroundColor: '#d8a62f' }}>
+          <div className="d-flex">
             <div className="d-flex flex-row justify-content-around">
-              <Link to="/patrimoine" className="btn btn-light">
+              <Link to="/patrimoine" className="btn"  style={{ fontSize: '1.5rem', color: 'white'}}>
                 Patrimoine
               </Link>
-              <Link to="/possession" className="btn btn-light">
+              <Link to="/possession" className="btn"  style={{ fontSize: '1.5rem', color: 'white' }}>
                 Possession
               </Link>
             </div>
-          </div>
         </div>
       </header>
 
       <Routes>
         <Route path="/" element={<PatrimoinePage />} />
         <Route path="/patrimoine" element={<PatrimoinePage />} />
-        <Route path="/possession" element={<ListPossessionPage />} />
+        <Route path="/possession" element={<PossessionListPage />} />
         <Route path="/possession/create" element={<CreatePossessionPage />} />
         <Route
           path="/possession/:libelle/update"
